3f797470a45f77e502f31a1d5b02d2d573628f1e
JULES <julestblt.dev@pm.me>
2023-07-26 17:48:45 +0200

    feat: refacto commands logic

---
+++ src/commands/hex.py
@@ -0,0 +1,24 @@
+from utils.parse_args import parse_args
+
+
+def hex_encode(value: str) -> str:
+    return value.encode('utf-8').hex()
+
+
+def hex_decode(data: str) -> str:
+    try:
+        return bytes.fromhex(data).decode('utf-8')
+    except ValueError:
+        return "Invalid Hex string"
+
+
+command_dict = {
+    "Encode": hex_encode,
+    "Decode": hex_decode
+}
+
+
+def hex_command(value: str = "", method: str = ""):
+    print("Hex Command")
+    command_choice, value = parse_args(value=value, method=method)
+    print(command_dict[command_choice](value))
--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 import questionary
 import pyfiglet
-from commands.base64 import base64_command
-from commands.url import url_command
-
-options_dict = {
-    "Base64": base64_command,
-    "Url": url_command,
-    "Quit": exit
-}
+from sys import argv
+from constants import options_dict, command_arg_dict
 
 
 def main():
     print(pyfiglet.figlet_format("CyberChef CLI", font="slant"))
     print("=====================================")
-    selected_option = questionary.select(
-        "Choose an option:", options_dict.keys()).ask()
-    options_dict[selected_option]()
+    if len(argv) > 1 and argv[1] in command_arg_dict:
+        command, method = command_arg_dict[argv[1]]
+        if method and len(argv) == 2:
+            command(method=method)
+        elif method and len(argv) > 2:
+            command(value=argv[2], method=method)
+        else:
+            command()
+    elif len(argv) == 1:
+        selected_option = questionary.select(
+            "Choose an option:", options_dict.keys()).ask()
+        options_dict[selected_option]()
 
 
 if __name__ == "__main__":
